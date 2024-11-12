ce8fe95b93c94e1c68db89715a45137fd4959388
parrrk <sohee1422@dongguk.edu>
2024-11-12 17:16:37 +0900

    main.py 수정

--- Subject_basic/main.py
+++ Subject_basic/main.py
@@ -11,9 +11,11 @@ def result():
     if request.method =='POST':
        result=dict()
        result['Name']=request.form.get('name')
-       # result['StudentNumber']=request.form.get('StudentNumber')
-       # result['languages'] = request.form.getlist('languages')
-       # result['languages'] =  ','.join(result['languages'])
+       result['StudentNumber']=request.form.get('StudentNumber')
+       result['gender']=request.form.get('gender')
+       result['major']=request.form.get('major')
+       result['languages'] = request.form.getlist('languages')
+       result['languages'] =  ','.join(result['languages'])
        return render_template('result.html',result=result)
 
 if __name__ =='__main__':
